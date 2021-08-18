037dda634774ed1d5394a6adca00023c1143ec2c
Ran Ding <randing89+github@gmail.com>
2021-08-18 08:56:09 -0700

    update

--- b/src/index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-  const [hello, world] = await Promise.all([
-    import('./a.js'),
-    import('./b.js')
-  ]);
-  console.log(hello + world);
-})();
--- b/webpack.config.js
+++ b/webpack.config.js
@@ -1,9 +1,71 @@
-const path = require('path');
-
+/** @type {import("../../../../").Configuration} */
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle0: {
+      import: "./index.js",
+      layer: "entry-layer",
+    },
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    pathinfo: "verbose",
+  },
+  module: {
+    rules: [
+      {
+        test: /module-layer-change/,
+        layer: "layer",
+      },
+      {
+        test: /module-other-layer-change/,
+        layer: "other-layer",
+      },
+      {
+        test: /module\.js$/,
+        issuerLayer: "other-layer",
+        loader: "./loader.js",
+        options: {
+          value: "other",
+        },
+      },
+      {
+        test: /module\.js$/,
+        issuerLayer: "layer",
+        loader: "./loader.js",
+        options: {
+          value: "ok",
+        },
+      },
+      {
+        test: /module\.js$/,
+        issuerLayer: "entry-layer",
+        loader: "./loader.js",
+        options: {
+          value: "entry",
+        },
+      },
+    ],
+  },
+  experiments: {
+    layers: true,
+  },
+  externals: [
+    {
+      external1: "var 42",
+      byLayer: {
+        layer: {
+          external1: "var 43",
+        },
+      },
+    },
+    {
+      external2: "var 42",
+      byLayer: (layer) => {
+        if (layer === "layer") {
+          return {
+            external2: "var 43",
+          };
+        }
+      },
+    },
+  ],
 };
